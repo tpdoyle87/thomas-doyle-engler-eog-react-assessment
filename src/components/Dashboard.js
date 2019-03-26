7f988d9237b0a6dab28ae5880a037f3a95b11b47
Thomas DoyleEngler <tpdoyle87@gmail.com>
2019-03-26 14:54:22 -0400

    1

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,10 +8,10 @@ import CardContent from "@material-ui/core/CardContent";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import LinearProgress from "@material-ui/core/LinearProgress"
+import LinearProgress from "@material-ui/core/LinearProgress";
 import { withStyles } from "@material-ui/core/styles";
 
-
+//
 class Dashboard extends Component {
 
   componentWillMount() {
